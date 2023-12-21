fd758697edd413cf2d0e759ed08281c673d79975
young <worksnr@outlook.com>
2023-12-21 16:26:22 +0800

    1

---
+++ Assets/SNRToy/Runtime/Script/SNRDefine.cs
@@ -0,0 +1,22 @@
+namespace SNRDefine
+{
+    public static class KWord
+    {
+        public const string CoroutineBehaviour = "CoroutineBehaviour";
+        public const string PkgRawFile = "PkgRawFile";
+        public const string PkgUnityObj = "PkgUnityObj";
+        public const string Hall = "Hall";
+        public const string DefaultPackage = "DefaultPackage";
+        public const string ServerAddress = "ServerAddress";
+        public const string PlayMode = "PlayMode";
+        public const string PackageName = "PackageName";
+        public const string BuildPipeline = "BuildPipeline";
+        public const string PackageVersion = "PackageVersion";
+
+    }
+
+
+
+
+
+}
